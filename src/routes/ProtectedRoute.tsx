38fc14d7dcec06c226d042f6f9c05fe8fc54847e
icbapp <icbapp.shopno@gmail.com>
2025-08-31 11:00:47 +0530

    set token to redirect time

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,7 +1,27 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const ProtectedRoute: React.FC = () => {
-  const auth_token = localStorage.getItem("auth_token");
+  const location = useLocation();
+
+  // read token from query string if present
+  const params = new URLSearchParams(location.search);
+  const tokenFromUrl = params.get("token");
+
+  // get whatever is currently stored
+  const storedToken = localStorage.getItem("auth_token");
+
+  useEffect(() => {
+    if (tokenFromUrl) {
+      // if token in URL, save it
+      localStorage.setItem("auth_token", tokenFromUrl);
+
+      // optional: remove token from URL after saving
+      window.history.replaceState({}, document.title, location.pathname);
+    }
+  }, [tokenFromUrl, location.pathname]);
+
+  const auth_token = tokenFromUrl || storedToken;
 
   if (!auth_token) {
     return <Navigate to="/signin" replace />;
@@ -9,6 +29,5 @@ const ProtectedRoute: React.FC = () => {
 
   return <Outlet />;
 };
-                                     
+
 export default ProtectedRoute;
- 
